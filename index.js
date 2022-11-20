42cbf133344282c5ffa9ab9f86e8cd576182435b
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-11-19 20:17:48 -0500

    connecting to mongodb

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
-app.use(bodyParser.json({limit: '30mb', extended: true}));
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+mongoose
+  .connect(
+    "mongodb+srv://MAGuzman:11dr0vv55@P!@cluster0.m55iqqj.mongodb.net/Linkup?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true}
+  )
+  .then(() => app.listen(5000, () => console.log("Listening")));

5b170f4b471ed63f9bd02b7ff1ecb07c20abe4c4
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-11-21 20:43:54 -0500

    set up env

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+dotenv.config()
 
 mongoose
   .connect(

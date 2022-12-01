279f5934572c8d6b9ff60855eb2645a8e9b9b21d
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-11-30 22:44:07 -0500

    created user model

---
+++ Models/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        firstname: {
+            type: String,
+            required: true
+        },
+        lastname: {
+            type: String,
+            required: true
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false
+        },
+        profilePicture: String,
+        
+    }
+)

dc6f4628230794f00cb66cc67596edc8014938cf
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-12-06 22:44:23 -0500

    added salt

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -6,6 +6,9 @@ import bcrypt from 'bcrypt';
 export const registerUser = async(req, res) => {
     const {username, password, firstname, lastname} = req.body;
 
+    const salt = await bcrypt.genSalt(10)
+    const hashedPass = await bcrypt.hash(password, salt)
+
     const newUser = new UserModel({username, password, firstname, lastname})
 
     try {

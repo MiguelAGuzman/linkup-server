246a396e8a0e81e94ecf7c20ef3b89d9493c5df7
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-12-08 20:57:05 -0500

    login user try catch

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -30,7 +30,11 @@ export const loginUser = async(req, res) => {
         if (user) {
             const validity = await bcrypt.compare(password, user.password)
 
-            
+            validity? res.status(200).json(user): res.status(400).json("Wrong Password")
+        } else {
+            res.status(404).json("User does not exist")
         }
+    } catch (error) {
+        res.status(500).json({message: error.message})
     }
 }

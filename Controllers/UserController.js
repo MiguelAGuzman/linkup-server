16641f28c45f90881fabef74ef60f44bd26a7d19
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-12-14 21:58:56 -0500

    user cont try catch

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -5,9 +5,13 @@ export const getUser = async(req, res) => {
     const id = req.params.id;
 
     try {
-        
+        const user = await UserModel.findById(id);
+
+        if(user) {
+            res.status(200).json(user);
+        }
     } catch (error) {
-        
+        res.status(500).json(error);
     }
     const user = await UserModel.findById(id);
 }

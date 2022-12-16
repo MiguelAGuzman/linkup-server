7acdfb9c0c5575913915ccba8d0457c750210c9e
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-12-15 23:49:49 -0500

    imported get user

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,9 +1,11 @@
 import Express from "express";
+import { getUser } from "../Controllers/UserController.js";
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    res.send("user route")
-})
+router.get('/:id', getUser)
+// router.get('/', async (req, res) => {
+//     res.send("user route")
+// })
 
 export default router;

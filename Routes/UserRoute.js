a2260193265e3a2c98a26f94bd199587f2e55423
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2023-02-06 16:28:18 -0500

    unfollow user route

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -7,7 +7,7 @@ router.get('/:id', getUser)
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser)
 router.put('/:id/follow', followUser)
-router.put('/:id', UnFollowUser)
+router.put('/:id/unfollow', UnFollowUser)
 
 // router.get('/', async (req, res) => {
 //     res.send("user route")

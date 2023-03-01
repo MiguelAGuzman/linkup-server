740bb4ac03040e45df88a89756d69acfe77f1053
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2023-03-01 11:37:32 -0500

    post cont

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -1,3 +1,8 @@
 import PostModel from "../Models/postModel.js";
 import mongoose from "mongoose";
 
+
+//create new post
+export const createPost = async (req, res) => {
+    const newPost = req.body
+}

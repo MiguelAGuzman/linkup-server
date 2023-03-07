21d2c260d8a7c6a646191b4df689ae403488e7cd
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2023-03-07 10:03:19 -0500

    post cont

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -10,6 +10,6 @@ export const createPost = async (req, res) => {
         await newPost.save()
         res.status(200).json("Post created successfully!")
     } catch (error) {
-        
+        res.status(500).json(error)
     }
 }

31f687c3f9d1e3c8e90aa6223a51f3c022c78f6f
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2023-03-09 08:02:37 -0500

    post route change

--- Routes/PostRoute.js
+++ Routes/PostRoute.js
@@ -1,8 +1,12 @@
 import express from "express";
+import { createPost } from "../Controller/PostController";
+
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-    res.send("Post Route")
-})
+router.get('/', createPost)
+
+// router.get('/', async (req, res) => {
+//     res.send("Post Route")
+// })
 
 export default router;

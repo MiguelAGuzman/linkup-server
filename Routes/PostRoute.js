59842409dbb9a259dfd9aef596c5382bf7c6b1e4
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2023-04-11 11:57:24 -0400

    post route put

--- Routes/PostRoute.js
+++ Routes/PostRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { createPost } from "../Controller/PostController.js";
+import { updatePost } from "../Controllers/PostController.js";
 
 const router = express.Router()
 
 router.post('/', createPost)
 router.get('/:id', getPost)
+router.put('/:id', updatePost)
 
 // router.get('/', async (req, res) => {
 //     res.send("Post Route")
